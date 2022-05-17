eb5a8e940ca54f529eccd3dc9c47beb1376cd2c3
Yahya Gilany <yahya.gilany@live.com>
2022-05-17 00:00:02 -0400

    update vulnerable packages and badge script

--- .scripts/badge.js
+++ .scripts/badge.js
@@ -1,11 +1,11 @@
-const git = require(`simple-git/promise`)(__dirname);
+const git = require(`simple-git`)(__dirname);
 const lineReplace = require(`line-replace`);
 
 async function main(){
   const remote = await git.getRemotes(true);
   const originRemote = remote.filter(r => r.name === `origin`)[0].refs.fetch.substring(4).slice(0,-4).replace(`:`,`/`);
 
-  const result = `[![Assignment Checks](https://${originRemote}/actions/workflows/classroom.yml/badge.svg)](https://${originRemote}/actions/workflows/classroom.yml)`;
+  const result = `[![GitHub Classroom Workflow](https://${originRemote}/actions/workflows/classroom.yml/badge.svg)](https://${originRemote}/actions/workflows/classroom.yml)`;
 
   lineReplace({
     file: `README.md`,
