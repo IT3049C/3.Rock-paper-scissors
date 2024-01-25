7d2df93e4620abcfc1d4b4c9566221bff1543248
Yahya Gilany <yahya.gilany@live.com>
2024-01-24 22:44:12 -0500

    updated tests

--- tests/4.controller.spec.ts
+++ tests/4.controller.spec.ts
@@ -31,5 +31,99 @@ test.describe("Layout Checks", () => {
     await expect(welcomeDivLocator).not.toBeVisible();
     await expect(gameDivLocator).toBeVisible();
   });
+
+  test(`When starting the game, the name is captured and displayed in the score tally paragraph`, async ({ page }) => {
+    const welcomeDivLocator = await page.locator(`#welcome-screen`);
+    const gameDivLocator = await page.locator(`#game-screen`);
+    const nameInput = await page.locator(`#welcome-screen input`);
+    const startButton = await page.locator(`#welcome-screen button`);
+    const scoreTally = await page.locator(`#game-screen #score`);
+
+    await expect(welcomeDivLocator).toBeVisible();
+    await expect(gameDivLocator).not.toBeVisible();
+
+    await nameInput.fill(`John`);
+    await startButton.click();
+
+    await expect(welcomeDivLocator).not.toBeVisible();
+    await expect(gameDivLocator).toBeVisible();
+
+    await expect(scoreTally).toHaveText(`John: 0 v CPU: 0`);
+
+    await page.reload();
+
+    await expect(welcomeDivLocator).toBeVisible();
+    await expect(gameDivLocator).not.toBeVisible();
+
+    await nameInput.fill(`Yahya`);
+    await startButton.click();
+
+    await expect(welcomeDivLocator).not.toBeVisible();
+    await expect(gameDivLocator).toBeVisible();
+
+    await expect(scoreTally).toHaveText(`Yahya: 0 v CPU: 0`);
+  });
+
+  test(`When selecting Rock as the user selection, a record is added to the game history`, async ({ page }) => {
+    const welcomeDivLocator = await page.locator(`#welcome-screen`);
+    const gameDivLocator = await page.locator(`#game-screen`);
+    const nameInput = await page.locator(`#welcome-screen input`);
+    const startButton = await page.locator(`#welcome-screen button`);
+    const goButton = await page.locator(`#game-screen button`);
+    const scoreTally = await page.locator(`#game-screen #score`);
+    const scoreHistory = await page.locator(`#game-screen #game-history`);
+
+    await expect(welcomeDivLocator).toBeVisible();
+    await expect(gameDivLocator).not.toBeVisible();
+
+    await nameInput.fill(`John`);
+    await startButton.click();
+
+    await expect(welcomeDivLocator).not.toBeVisible();
+    await expect(gameDivLocator).toBeVisible();
+
+    await expect(scoreTally).toHaveText(`John: 0 v CPU: 0`);
+
+    // each user selection appears properly in the history
+    for (const selection of [`rock`, `paper`, `scissors`]) {
+      await page.selectOption(`#game-screen select`, selection);
+      await goButton.click();
+      const lastHistoryLog = await scoreHistory.getByRole(`listitem`).last().textContent();
+      await expect(lastHistoryLog).toContain(`John selected ${selection}`);
+    };
+  });
+
+  test(`When selecting an option, depending on the cpu selection, the score should update accordingly`, async ({ page }) => {
+    const welcomeDivLocator = await page.locator(`#welcome-screen`);
+    const gameDivLocator = await page.locator(`#game-screen`);
+    const nameInput = await page.locator(`#welcome-screen input`);
+    const startButton = await page.locator(`#welcome-screen button`);
+    const goButton = await page.locator(`#game-screen button`);
+    const scoreTally = await page.locator(`#game-screen #score`);
+    const scoreHistory = await page.locator(`#game-screen #game-history`);
+
+    await expect(welcomeDivLocator).toBeVisible();
+    await expect(gameDivLocator).not.toBeVisible();
+
+    await nameInput.fill(`John`);
+    await startButton.click();
+
+    await expect(welcomeDivLocator).not.toBeVisible();
+    await expect(gameDivLocator).toBeVisible();
+
+    await expect(scoreTally).toHaveText(`John: 0 v CPU: 0`);
+    
+    let doneLooping = false;
+    while (!doneLooping) {
+      await page.selectOption(`#game-screen select`, `rock`);
+      await goButton.click();
+      
+      const lastHistoryLog = await scoreHistory.getByRole(`listitem`).last().textContent();
+      await expect(lastHistoryLog).toContain(`John selected rock`);
+      (await scoreTally.textContent()).includes(`v CPU: 3`) ? doneLooping = true : doneLooping = false;
+    }
+
+  });
+
   
 });
