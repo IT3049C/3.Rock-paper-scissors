593ce2d3750c6aa9ee7cb985116aeffa4d670de7
Yahya Gilany <yahya.gilany@live.com>
2024-01-24 20:12:24 -0500

    layout tests

--- tests/1.layout.spec.ts
+++ tests/1.layout.spec.ts
@@ -9,7 +9,7 @@ test.describe("Layout Checks", () => {
 
   test("displays a form", async ({ page }) => {
     await expect(page).toHaveTitle(/rock paper scissors/i);
-    await expect(page.locator(`h1`)).toHaveText(`Rock Paper Scissors`);
+    await expect(page.getByRole(`heading`)).toContainText(/rock paper scissors/i);
   });
 
   test(`There exists 2 divs with classes welcome-screen and game-screen`, async ({ page }) => {
@@ -18,12 +18,11 @@ test.describe("Layout Checks", () => {
     await expect(page.locator(`#game-screen`)).toBeTruthy();
   });
 
-  test(`welcome screen includes a form for the name`, async ({ page }) => {
+  test(`welcome screen includes a form for the name, and a button to start game`, async ({ page }) => {
     const formLocator = await page.locator(`#welcome-screen form`);
     await expect(formLocator).toBeTruthy();
 
     await expect(formLocator.getByLabel(/your name/i)).toBeTruthy();
-    await expect(formLocator.locator(`label`)).toContainText(/your name/i);
     await expect(formLocator.getByLabel(/your name/i)).toHaveAttribute(`placeholder`, /enter name here/i);
 
     await expect(formLocator.getByRole(`button`)).toBeTruthy();
@@ -31,15 +30,17 @@ test.describe("Layout Checks", () => {
     await expect(formLocator.getByRole(`button`)).toHaveAttribute(`id`, `start-game-button`);
   });
 
-  test(`game screen includes a form for the game`, async ({ page }) => {
+  test(`game screen has space for score tally`, async ({ page }) => {
     const gameScreenScoreTallyDiv = await page.locator(`#game-screen > div`);
     const gameScreenScoreTallyP = await gameScreenScoreTallyDiv.locator(`p`);
-
+    
     await expect(gameScreenScoreTallyDiv).toHaveAttribute(`id`, `score-tally`);
     await expect(gameScreenScoreTallyP).toHaveAttribute(`id`, `score`);
     await expect(gameScreenScoreTallyP).toContainText(/user: 0 v cpu: 0/i);
-
-    const formLocator = await page.locator(`#game-screen > form`);
+  });
+    
+  test(`game screen includes a form with a dropdown and a button`, async ({ page }) => {
+    const formLocator = await page.locator(`#game-screen form`);
     await expect(formLocator).toBeTruthy();
 
     await expect(formLocator.getByLabel(/select your choice/i)).toBeTruthy();
@@ -47,8 +48,8 @@ test.describe("Layout Checks", () => {
     await expect(formLocator.getByLabel(/select your choice/i).locator(`option`)).toContainText([/rock/i, /paper/i, /scissors/i]);
 
     await expect(formLocator.getByRole(`button`)).toBeTruthy();
-    await expect(formLocator.getByRole(`button`)).toContainText(/go/i);
-    await expect(formLocator.getByRole(`button`)).toHaveAttribute(`id`, `go-button`);
+    await expect(formLocator.getByRole(`button`, {includeHidden: true})).toHaveAttribute(`id`, `go-button`);
+    await expect(formLocator.getByRole(`button`, {includeHidden: true})).toContainText(/go/i);
 
     await expect(formLocator.locator(`#game-history`)).toBeTruthy();
   });
